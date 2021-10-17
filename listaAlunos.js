91ccf7dc6b3bcdbf252652936b6d235d83d0084b
Jonatas Nascimento <jts.nascimento17@hotmail.com>
2021-10-17 09:21:29 -0300

    Criação de lista - ER2

---
+++ listaAlunos.js
@@ -0,0 +1,22 @@
+let numeroAlunos = 5;
+
+// console.log("Jonatas")
+// console.log("Jonatas")
+// console.log("Jonatas")
+// console.log("Jonatas")
+// console.log("Jonatas")
+
+for(let contador = 0; contador < numeroAlunos; contador++) {
+    //console.log ("Jonatas - For");
+    console.log(contador)
+    
+    if(contador == 0) {
+        console.log("ZERO");
+}  else if (contador % 2 == 1) {
+        console.log ("ÍMPAR")            
+}   else {
+        console.log ("PAR")
+}
+        
+    }
+
